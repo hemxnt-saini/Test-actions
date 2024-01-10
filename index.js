6da4df636a1d8dac594117ee25903095f02beef0
hemxnt.saini <hemant@hybr1d.io>
2024-01-10 12:34:43 +0530

    hello world feature

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.set('Content-Type', 'text/plain');
-  res.send('Hello World from Dev1');
+  res.send('Hello world feature');
 });
 
 module.exports = app.listen(8080, () => {
